6b6ceda032592e453f0b9596ef2e62a91ffff22f
Diego Fernando <diegofernandofonseca@hotmail.com>
2024-10-30 08:09:58 -0500

    /** Cambios  Realizados **/

---
+++ src/WebPersonal/Controllers/PerfilPersonalController.cs
@@ -0,0 +1,59 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using WebPersonal.Shared.v1;
+
+namespace WebPersonal.Controllers
+{
+
+    
+    [Route("api/v{version:int}/[Controller]")]
+    [ApiController]
+    [ApiVersion("1")]
+
+    public class PerfilPersonalController : ControllerBase {
+
+
+        /* ------ Leeer------- */
+
+        [HttpGet("LeerPerfil/{id}")]
+        public string Get(int id)
+        {
+            return id switch
+            {
+
+                1 => "Diego",
+                2 => "Ing Sistemas",
+                _ => throw new NotSupportedException("El id no es valido")
+
+
+            };
+
+        }
+
+
+        /* ----- Guardar ----- */
+
+        [HttpPost("Guardar")]
+        public IActionResult Post(PerfilDto perfil)
+        {
+            PerfilDto _perfilDTO = new PerfilDto
+            {
+                Nombre = perfil.Nombre,
+                Direccion = perfil.Direccion,
+                Telefono = perfil.Telefono,
+
+            };
+          
+            return StatusCode(StatusCodes.Status200OK,  _perfilDTO );
+
+        }
+
+
+
+    }
+
+
+
+}
+
+
---
+++ src/WebPersonal/Shared/v2/PerfilDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebPersonal.Shared.v2
+{
+    public class PerfilDto
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+
+
+    }
+}

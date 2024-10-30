678e90e7b0256bf890124a5ae2b9daf5e1920610
Diego Fernando <diegofernandofonseca@hotmail.com>
2024-10-30 11:09:07 -0500

    Nuevo Proyecto

---
+++ ApiCrudASP/ASPWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
